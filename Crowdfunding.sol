b275116e96619d8e3ebb17f57a2b1f3d6fd88cb7
yngfng <yngfng@gmail.com>
2017-06-21 07:54:59 +0800

    fix

--- Crowdfunding.sol
+++ Crowdfunding.sol
@@ -6,7 +6,7 @@ contract Token {
     function transferFrom(address from, address to, uint256 value) returns (bool success);
     function approve(address spender, uint256 value) returns (bool success);
 
-    function totalSupply() constant returns (uint256 supply) {}
+    //function totalSupply() constant returns (uint256 supply) {}
     function balanceOf(address owner) constant returns (uint256 balance);
     function allowance(address owner, address spender) constant returns (uint256 remaining);
 
